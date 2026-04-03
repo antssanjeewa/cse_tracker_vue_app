98ffa94a8ecc70272e57f8691449baab1c49d893
antssanjeewa94 <antssanjeewa94@gmail.com>
2026-04-03 12:17:24 +0530

    add: deploy files

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  base: '/cse_tracker_vue_app/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
